3965bce9b558fd716382b3f823e231a036465071
«Azat» <azaatyusupoff@yandex.ru>
2023-03-03 18:50:39 +0400

    Задание 2

---
+++ Lesson1/1_2/Program.cs
@@ -0,0 +1,38 @@
+﻿// 2. Напишите программу , которая будет выдавать название дня
+//  недели по заданному номеру .
+
+string s_a = Console.ReadLine()!;
+
+if (s_a=="1")
+{
+    Console.WriteLine("Понедельник");
+}
+
+else if (s_a=="2")
+{
+    Console.WriteLine("Вторник");
+}
+else if (s_a=="3")
+{
+    Console.WriteLine("Среда");
+}
+else if (s_a=="4")
+{
+    Console.WriteLine("Четверг");
+}
+else if (s_a=="5")
+{
+    Console.WriteLine("Пятница");
+}
+else if (s_a=="6")
+{
+    Console.WriteLine("Суббота");
+}
+else if (s_a=="7")
+{
+    Console.WriteLine("Воскресенье");
+}
+else
+{
+    Console.WriteLine("Error");
+}
