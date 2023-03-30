1c552f34c24dd06f8af4242548f4e4db38647ca2
«Azat» <azaatyusupoff@yandex.ru>
2023-03-30 21:36:56 +0400

    2zadanie

---
+++ dz_na_1.04/1_1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+//натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = SumNaturalNumbers(m, n);
+        Console.WriteLine("Сумма натуральных элементов от {0} до {1} равна {2}", m, n, sum);
+
+        Console.ReadKey();
+    }
+
+    static int SumNaturalNumbers(int m, int n)
+    {
+        if (m == n)
+        {
+            return m;
+        }
+
+        return m + SumNaturalNumbers(m + 1, n);
+    }
+}
